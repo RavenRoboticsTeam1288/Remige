b7d3130ad503314512263dce1edcf677c1c58cc0
Jacob Fischer <jacob.t.fischer@gmail.com>
2016-02-20 22:24:20 -0600

    Lots of fixes and tweaks
    * buttons show better when disabled, and are by default
    * connection statuses color coded
    * added error images
    * buttons cannot be clicked when maximized
    * layout tweaks

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
     var $body = $("body");
     var $form = $("#remige");
-    var $formInputs = $("input", $form); // Note: we cache this, so if you are dynamically adding in inputs don't cache it
+    var $formInputs = $("input", $form).prop("disabled", true); // Note: we cache this, so if you are dynamically adding in inputs don't cache it
 
     var $robotConnection = $("#robot-connection");
     var $serverConnection = $("#server-connection");
 
-    function updateSeverConnection(str) {
-        $serverConnection.html(str);
+    function updateSeverConnection(str, onlyClass) {
+        $serverConnection
+            .html(str)
+            .attr("class", "")
+            .addClass(onlyClass);
     };
 
     var $imgs = [];
     function updateRobotConnection(str) {
-        str = str.charAt(0).toUpperCase() + str.slice(1); // capitalize the first letter
-        $robotConnection.html(str);
+        var cap = str.charAt(0).toUpperCase() + str.slice(1); // capitalize the first letter
+        $robotConnection.html(cap);
+        $robotConnection.addClass(str);
+
         for(var i = 0; i < $imgs.length; i++) {
             var $img = $imgs[i];
-            $img.attr("src", $img.attr("src")); // reload
+            $img
+                .removeClass("errored")
+                .attr("src", $img.realSRC); // reload
         }
     };
 
@@ -38,18 +45,31 @@ $(document).ready(function() {
     };
 
     var $maximize = $("#maximize").on("click", function() {
-        $body.toggleClass("maximized");
+        if($body.hasClass("maximized")) {
+            $form.show();
+        }
+        else {
+            setTimeout(function() {
+                $form.hide();
+            }, 1000);
+        }
+
+        setTimeout(function() {
+            $body.toggleClass("maximized");
+        }, 10);
     });
 
-    updateSeverConnection("Connecting...");
+    updateSeverConnection("Connecting...", "connecting");
     $.getJSON("/config.json", function(config) {
-        updateSeverConnection("Establishing WS Connection...");
+        updateSeverConnection("Establishing WS Connection...", "connecting");
 
-        var wsClient = new WebSocket("ws://localhost:" + config["ws_port"]);
+        var wsClient = new WebSocket("ws://" + window.location.hostname + ":" + config["ws_port"]);
 
         var updating = false; // poor man's mutex lock
+        var connected = false;
         wsClient.onopen = function (e) {
-            updateSeverConnection("Connected");
+            connected = true;
+            updateSeverConnection("Connected", "connected");
 
             var send = function(data) {
                 var str = JSON.stringify(data);
@@ -67,12 +87,14 @@ $(document).ready(function() {
         wsClient.onmessage = function(e) {
             console.log("GOT: <- " + e.data);
 
+            $formInputs.prop("disabled", false);
+
             var parsed;
             try {
                 parsed = JSON.parse(e.data);
             }
             catch(err) {
-                updateSeverConnection("Sent invalid JSON...");
+                updateSeverConnection("Invalid JSON Sent", "disconnected");
                 return;
             }
 
@@ -98,26 +120,35 @@ $(document).ready(function() {
 
         wsClient.onerror = function() {
             console.log("socket errored");
-            updateSeverConnection("Errored");
+            updateSeverConnection("Errored", "disconnected");
             $formInputs.prop('disabled', true);
         };
 
         wsClient.onclose = function() {
             console.log("connection closed");
-            updateSeverConnection("Closed");
+            updateSeverConnection(connected ? "Connection Closed" : "Could Not Connect", "disconnected");
             $formInputs.prop('disabled', true);
         };
 
-        
         if(config.image_streams) {
             var $streams = $("#streams");
             for(var i = 0; i < config.image_streams.length; i++) {
-                var steam = config.image_streams[i];
+                var stream = config.image_streams[i];
+                var title = stream.title || "Camera " + (i+1);
 
-                $imgs.push($("<img>")
-                    .attr("src", "http://" + config.robot_ip + ":" + steam.port + steam.path)
+                var $img = $("<img>")
+                    .attr("src", "http://" + config.robot_ip + ":" + stream.port + stream.path)
+                    .attr("alt", title + " (OFFLINE)")
+                    .attr("title", title)
                     .appendTo($streams)
-                );
+                    .on("error", function(e) { // it failed to load, probably because we are disconnected from the Robot
+                        $(this)
+                            .attr("src", "/blank.png")
+                            .addClass("errored");
+                    });
+
+                $img.realSRC = $img.attr("src");
+                $imgs.push($img)
             }
         }
     });
